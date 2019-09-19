1ca59e24903538714e19c7c48749a6df7e36b031
Mitul Jindal <mituljindal@gmail.com>
2019-09-19 13:58:31 +0530

    All view created

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,44 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from polls.models import Question, Choice
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    print(question)
+    try:
+        print(question.choice_set)
+        selected_choice = question.choice_set.get(pk=request.POST['Choice'])
+        print(selected_choice)
+    except (KeyError, Choice.DoesNotExist):
+        print("In except")
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't choose",
+        })
+    else:
+        print("In else")
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
